a318e234b5fbc4254304f0b417118db630b98e8c
poitrew <dtdat101101@gmail.com>
2022-06-09 20:27:56 +0700

    finished exercise 3.3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id) 
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
